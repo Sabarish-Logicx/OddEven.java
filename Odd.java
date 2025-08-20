7dc3ac28a8fdbad2eacf2006ab65e53954cf653f
Sabareeswaran S <sabarishanmugam357@gmail.com>
2025-08-20 23:39:55 +0530

    created Odd.java

---
+++ Odd.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Odd{
+    static Scanner Sc=new Scanner(System.in);
+    public static void main (String args[]){
+    System.out.println("Enter a Number:");
+    int num=Sc.nextInt();
+    if (isEvenOdd(num)){
+        System.out.println("Even");
+    }else{
+        System.out.println("Odd");
+    
+    }}
+    public static boolean isEvenOdd (int num)
+    {
+        return num%2==0;
+    }
+}
