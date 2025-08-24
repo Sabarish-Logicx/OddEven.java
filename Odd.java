e79b49c6feb5583d6126d33b07d6215a41a16264
Sabareeswaran S <sabarishanmugam357@gmail.com>
2025-08-24 23:55:33 +0530

    Modified //

--- Odd.java
+++ Odd.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Odd{
     static Scanner Sc=new Scanner(System.in);
     public static void main (String args[]){
-    System.out.println("Enter a Number:");
+    System.out.println("Enter a Number:"); //10
     int num=Sc.nextInt();
     if (isEvenOdd(num)){
         System.out.println("Even");
@@ -12,6 +12,6 @@ public class Odd{
     }}
     public static boolean isEvenOdd (int num)
     {
-        return num%2==0;
+        return num%2==0; //Even
     }
 }
