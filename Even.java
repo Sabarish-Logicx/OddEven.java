d82606ff623f7b05ea255be947181cdd560f7c15
Sabareeswaran S <sabarishanmugam357@gmail.com>
2025-08-28 23:32:22 +0530

    Modify

--- Even.java
+++ Even.java
@@ -2,9 +2,10 @@ class Even{
     public static void main(String[] args){
         int n=15;
         if(n%2==0){
-            System.out.println("Yes");
+            System.out.print("The Number is odd");
         }else{
-            System.out.println("No");
-            }
+            System.out.print("The Number is Even");
+            
         }
     }
+}
