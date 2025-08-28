fb344e347ac536b626113f69bcd10010030a3de6
Sabareeswaran S <sabarishanmugam357@gmail.com>
2025-08-28 23:26:42 +0530

    Added

---
+++ Even.java
@@ -0,0 +1,10 @@
+class Even{
+    public static void main(String[] args){
+        int n=15;
+        if(n%2==0){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+            }
+        }
+    }
